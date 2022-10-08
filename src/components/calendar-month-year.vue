087dfc045f11014de2df4fddbc89cd26e5e712ec
Gustavo Maysonnave Franck <gmaysonnavefranck@gmail.com>
2022-10-08 09:16:39 -0300

    adding navigation and top informations

---
+++ src/components/calendar-month-year.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{cYearMonth}}</h1>
+</template>
+
+<script>
+const MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+export default {
+  props: {
+    year: {
+      type: Number,
+      required: true 
+    },
+    month: {
+      type: Number,
+      required: true 
+    },
+  },
+  computed: {
+    cYearMonth() {
+      return `${this.getMonthName(this.month)}, ${this.year}`
+    }
+  },
+  methods: {
+    getMonthName(monthNumber) {
+      return MONTH_NAMES[monthNumber];
+    }
+  },
+}
+</script>
---
+++ src/components/calendar-navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-btn
+      icon
+      @click="() => changeMonth(-1)"
+    >
+      <v-icon>
+        mdi-chevron-left
+      </v-icon>
+    </v-btn>
+    <v-btn 
+      color="accent" 
+      depressed 
+      small 
+      outlined 
+      class="mx-3"
+      @click="() => changeMonth(0)"
+    >
+      Current Month
+    </v-btn>
+    <v-btn
+      icon
+      @click="() => changeMonth(1)"
+    >
+      <v-icon>
+        mdi-chevron-right
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeMonth(changeValue) {
+      this.$emit('changeMonth', changeValue) // -1 to previous, 0 to return to current, 1 to next
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
