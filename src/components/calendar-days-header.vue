5f1f089f83d2b77d659700be349c754643d5daf1
Gustavo Maysonnave Franck <gmaysonnavefranck@gmail.com>
2022-10-07 22:00:33 -0300

    creating calendar header with the name of the days

---
+++ src/components/calendar-days-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <ol class="day-text body-1">
+    <li v-for="day in days" :key="day"> {{ day }} </li>
+  </ol>
+</template>
+
+<script>
+const CONSTANT_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+export default {
+  computed: {
+    days() {
+      return CONSTANT_DAYS 
+    }
+  },
+}
+</script>
+
+<style scoped>
+.day-text {
+  background-color:rgb(40, 71, 161);
+  color: white;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  list-style-type: none;
+  text-align: center;
+}
+
+</style>
