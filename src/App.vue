a9d5195478aa4677ffae2917dd7913f8d5c81fb1
Gustavo Maysonnave Franck <gmaysonnavefranck@gmail.com>
2022-10-07 21:31:35 -0300

    creating vue project; adding vuetify, vuex and jest

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <calendar-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CalendarView from './components/calendar-view';
+
+export default {
+  name: 'App',
+
+  components: {
+    CalendarView,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
